fdeffa103b6ccc65a96351a298f96e08bf72f881
Ivan Vukorepa <ivukor00@fesb.hr>
2017-09-29 15:03:00 +0200

    Finished

--- scripts/app/routes/home/homeController.js
+++ scripts/app/routes/home/homeController.js
@@ -2,7 +2,7 @@
 
     $scope.AllNews = GetAllNewsService.getAllNews();
 
-    $scope.slicedNews = _.chunk($scope.AllNews, 2);
+    $scope.slicedNews = _.chunk($scope.AllNews, 3);
     $scope.News = $scope.slicedNews[0];
 
     $scope.numberOfNewsShown = 1;
@@ -15,7 +15,7 @@
 
     $scope.showMore = function () {
         $scope.News = _.concat($scope.News, $scope.slicedNews[$scope.numberOfNewsShown++]);
-        if ($scope.AllNews.length <= $scope.numberOfNewsShown * 2)
+        if ($scope.AllNews.length <= $scope.numberOfNewsShown * 3)
             $scope.AllNewsAreShown = true;
     }
 
